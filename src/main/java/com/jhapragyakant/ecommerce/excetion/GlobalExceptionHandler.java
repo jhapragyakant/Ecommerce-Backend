3b976d77a0292619b1d57b36a4ebf66f08880219
Pragya <jhapragyakant@gmail.com>
2023-08-25 16:11:18 +0530

    added MethodArgumentNotValidException handle in GlobalExceptionHandler Class.

---
+++ src/main/java/com/jhapragyakant/ecommerce/excetion/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.jhapragyakant.ecommerce.excetion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String,String> resp = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String filename = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+            resp.put(filename,errorMessage);
+        });
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
+}
