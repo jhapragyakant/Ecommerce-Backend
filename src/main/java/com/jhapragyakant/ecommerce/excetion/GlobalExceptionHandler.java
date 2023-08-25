9ce17c27c29e392d290d34953374b8fbe524607b
Pragya <jhapragyakant@gmail.com>
2023-08-25 19:45:24 +0530

    added ResourceNotFoundException

--- src/main/java/com/jhapragyakant/ecommerce/excetion/GlobalExceptionHandler.java
+++ src/main/java/com/jhapragyakant/ecommerce/excetion/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jhapragyakant.ecommerce.excetion;
 
+import com.jhapragyakant.ecommerce.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -23,5 +24,11 @@ public class GlobalExceptionHandler {
             resp.put(filename,errorMessage);
         });
         return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        return new ResponseEntity<>(new ApiResponse(false,message), HttpStatus.NOT_FOUND);
     }
 }
