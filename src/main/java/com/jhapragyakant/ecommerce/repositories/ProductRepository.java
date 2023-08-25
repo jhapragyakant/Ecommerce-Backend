bbf7c4e63b31ad0660196dea80886081b727e487
Pragya <jhapragyakant@gmail.com>
2023-08-25 19:46:58 +0530

    rectified type of id as String in place of UUID.

--- src/main/java/com/jhapragyakant/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/jhapragyakant/ecommerce/repositories/ProductRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, String> {
     Boolean existsByProductName(String productName);
 }
